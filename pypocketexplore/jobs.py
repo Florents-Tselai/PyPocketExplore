51d920b672bf50952a73ba2c90a726b920d8466a
Florents Tselai <florents.tselai@gmail.com>
2017-05-21 12:21:11 +0300

    Add job to download articles

--- pypocketexplore/jobs.py
+++ pypocketexplore/jobs.py
@@ -8,6 +8,53 @@ from time import sleep
 from redis import StrictRedis
 import rq
 
+import redis
+from threading import Thread
+import pickle
+from newspaper import Article
+from collections import deque
+from queue import Queue
+from time import time
+import requests as req
+
+from rq.decorators import job
+
+
+@job('pocket.download', connection=redis.StrictRedis(), timeout=-1)
+def download_item(item_id, item_url, redis_con=redis.StrictRedis()):
+    item = {
+        'item_id': item_id,
+        'item_url': item_url
+    }
+    data = {
+        'item_id': item.get('item_id'),
+        'item_url': item.get('item_url'),
+        'response': None,
+        'article': None
+    }
+
+    try:
+        # Make request
+        data['response'] = req.get(data.get('item_url'))
+
+        # Download article
+        try:
+            a = Article(item_url)
+            data['article'] = a
+            a.download()
+            a.parse()
+        except Exception as e:
+            print('Item: {} | Url: {} | {}'.format(data.get('item_id'), data.get('item_url'), e))
+            raise e
+
+        redis_con.set('pocket:item:{}'.format(item.get('item_id')),
+                           pickle.dumps(data)
+                           )
+
+    except Exception as e:
+        print('Item: {} | Url: {} | {}'.format(data.get('item_id'), data.get('item_url'), e))
+        raise e
+
 
 def extract_topic_items(topic):
     r = StrictRedis()
@@ -21,6 +68,7 @@ def extract_topic_items(topic):
 
     db = MongoClient(MONGO_URI).get_default_database()
     resp = req.get('http://localhost:5000/api/topic/{}'.format(topic))
+    print(resp.content)
     data = resp.json()
     related_topics = data.get('related_topics')
 
