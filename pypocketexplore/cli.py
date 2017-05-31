0aea9169d8904f215b5cccaa48ec8d1c106758de
Florents Tselai <florents.tselai@gmail.com>
2017-05-31 18:54:41 +0300

    Add cli

---
+++ pypocketexplore/cli.py
@@ -0,0 +1,31 @@
+import json
+from logging import ERROR
+
+import click
+
+from pypocketexplore.parser import PocketTopicScraper, logger
+
+logger.setLevel(ERROR)
+
+
+@click.command('topic')
+@click.argument('label', nargs=-1)
+@click.option('--limit', default=100, help='Limit items to download')
+@click.option('--out', default='topics.json', help='JSON output fp')
+@click.option('--parse', is_flag=True, help='If set, also parses the html and runs it through NLTK')
+def topic(label, limit, out, parse):
+    items = []
+    for l in label:
+        scraper = PocketTopicScraper(l, limit=limit, parse=parse)
+        items.extend([i.to_dict() for i in scraper.scrap().items])
+    json.dump(items, open(out, encoding='utf-8', mode='w'),
+              indent=4,
+              sort_keys=True)
+
+
+def main():
+    topic()
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='PyPocketExplore',
     version='1.0.0',
     packages=['pypocketexplore', ],
+    entry_points={
+        'console_scripts': ['pypocketexplore=pypocketexplore.cli:main'],
+    },
     license='The MIT License (MIT) Copyright © 2017 Florents Tselai.',
     description='Unofficial API to Pocket Explore data',
     long_description=open('README.md', 'r').read(),
     author='Florents Tselai',
     author_email='florents.tselai@gmail.com',
     url='https://github.com/Florents-Tselai/PyPocketTopics',
+    install_requires=requirements
 )
